ab439cae3705e11bb4e58c8e5ea2a1e732117719
Shaun McKinnon <shaun.mckinnon@georgiancollege.ca>
2020-11-18 18:38:17 -0500

    finished project part 1

--- index.js
+++ index.js
@@ -24,10 +24,21 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Setup our session
-
+const session = require('express-session');
+app.use(session({
+  resave: true,
+  saveUninitialized: false,
+  secret: 'any salty secret here'
+}));
 
 // Setting up Passport
-
+const passport = require('passport');
+app.use(passport.initialize());
+app.use(passport.session());
+const User = require('./models/user');
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 // Setting up Passport JWT
 
@@ -42,4 +53,5 @@ const { handle404s, errorHandler } = require('./errorHandling');
 app.use(handle404s);
 app.use(errorHandler);
 
-app.listen(4000, () => console.log("Always watching... on port 4000"));
+const port = process.env.PORT || 4000;
+app.listen(port, () => console.log(`Always watching... on port ${port}`));
